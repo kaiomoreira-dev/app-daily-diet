2c15b2d570259c2fff2dbfafd51c7372dedd260f
Gustavo Reimche <gustavoprdevgmail.com>
2023-07-18 16:35:30 -0300

    update: new snack page component

---
+++ src/app/views/new-snack/new-snack.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { ISnack } from "src/app/models/snack.model";
+
+@Component({
+	selector: "app-new-snack",
+	templateUrl: "./new-snack.component.html",
+	styleUrls: ["./new-snack.component.scss"],
+})
+export class NewSnackComponent {}
+

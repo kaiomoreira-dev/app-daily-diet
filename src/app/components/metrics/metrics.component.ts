8193f836f2112f20c7962e380eaa83aaffd5137e
Gustavo Reimche <gustavoprdevgmail.com>
2023-07-18 16:31:29 -0300

    feat: create metrics component

---
+++ src/app/components/metrics/metrics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-metrics",
+	templateUrl: "./metrics.component.html",
+	styleUrls: ["./metrics.component.scss"],
+})
+export class MetricsComponent {
+	iconColor = "#639339";
+	success = "#E5F0DB";
+}
+

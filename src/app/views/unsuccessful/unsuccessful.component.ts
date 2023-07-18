281889c03831614a94547ffed5119e1721b6c74c
Gustavo Reimche <gustavoprdevgmail.com>
2023-07-18 16:36:51 -0300

    feat: create unseccessful component page

---
+++ src/app/views/unsuccessful/unsuccessful.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unsuccessful',
+  templateUrl: './unsuccessful.component.html',
+  styleUrls: ['./unsuccessful.component.scss']
+})
+export class UnsuccessfulComponent {
+
+}
